878967a2883900b125b48d608a886dc7facca163
Charlie Pyle <cpyle@usc.edu>
2019-11-09 17:27:16 -0800

    can now upload images to firebase storage

--- chrysalis/App.js
+++ chrysalis/App.js
@@ -1,47 +1,198 @@
-/**
- * Sample React Native App with Firebase
- * https://github.com/invertase/react-native-firebase
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  //AsyncStorage,
+  Dimensions,
+  ScrollView
+} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+import AsyncStorage from '@react-native-community/async-storage';
+import firebase from 'react-native-firebase';
+import uuid from 'uuid/v4'; // Import UUID to generate UUID
 
-import firebase from '@react-native-firebase/app';
-
-// TODO(you): import any additional firebase services that you require for your app, e.g for auth:
-//    1) install the npm package: `yarn add @react-native-firebase/auth@alpha` - you do not need to
-//       run linking commands - this happens automatically at build time now
-//    2) rebuild your app via `yarn run run:android` or `yarn run run:ios`
-//    3) import the package here in your JavaScript code: `import '@react-native-firebase/auth';`
-//    4) The Firebase Auth service is now available to use here: `firebase.auth().currentUser`
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu',
-});
-
-const firebaseCredentials = Platform.select({
-  ios: 'https://invertase.link/firebase-ios',
-  android: 'https://invertase.link/firebase-android',
-});
-
-type Props = {};
-
-export default class App extends Component<Props> {
+const options = {
+  title: 'Select Image',
+  storageOptions: {
+    skipBackup: true,
+    path: 'images'
+  }
+};
+const ImageRow = ({ image, windowWidth, popImage }) => (
+  <View>
+    <Image
+      source={{ uri: image }}
+      style={[styles.img, { width: windowWidth / 2 - 15 }]}
+      onError={popImage}
+    />
+  </View>
+);
+export default class App extends Component {
+  state = {
+    imgSource: '',
+    uploading: false,
+    progress: 0,
+    images: []
+  };
+  componentDidMount() {
+    let images;
+    alert('did mount');
+    AsyncStorage.getItem('images')
+      .then(data => {
+        images = JSON.parse(data) || [];
+        this.setState({
+          images: images
+        });
+      })
+      .catch(error => {
+         console.log(error);
+      });
+  }
+  /**
+   * Select image method
+   */
+  pickImage = () => {
+    alert('will break!');
+    ImagePicker.showImagePicker(options, response => {
+      if (response.didCancel) {
+        alert('You cancelled image picker 😟');
+      } else if (response.error) {
+        alert('And error occured: ', response.error);
+      } else {
+        const source = { uri: response.uri };
+        this.setState({
+          imgSource: source,
+          imageUri: response.uri
+        });
+      }
+    });
+  };
+  /**
+   * Upload image method
+   */
+  uploadImage = () => {
+    //const ext = this.state.imageUri.split('.').pop(); // Extract image extension
+    const filename = 'fasdfkjhasdf';
+    //const filename = `${uuid()}.${ext}`; // Generate unique name
+    this.setState({ uploading: true });
+    firebase
+      .storage()
+      .ref(`tutorials/images/${filename}`)
+      .putFile(this.state.imageUri)
+      .on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        snapshot => {
+          let state = {};
+          state = {
+            ...state,
+            progress: (snapshot.bytesTransferred / snapshot.totalBytes) * 100 // Calculate progress percentage
+          };
+          if (snapshot.state === firebase.storage.TaskState.SUCCESS) {
+            const allImages = this.state.images;
+            allImages.push(snapshot.downloadURL);
+            state = {
+              ...state,
+              uploading: false,
+              imgSource: '',
+              imageUri: '',
+              progress: 0,
+              images: allImages
+            };
+            AsyncStorage.setItem('images', JSON.stringify(allImages));
+          }
+          this.setState(state);
+        },
+        error => {
+          unsubscribe();
+          alert('Sorry, Try again.');
+        }
+      );
+  };
+  /**
+   * Remove image from the state and persistance storage
+   */
+  removeImage = imageIndex => {
+    let images = this.state.images;
+    images.pop(imageIndex);
+    this.setState({ images });
+    AsyncStorage.setItem('images', JSON.stringify(images));
+  };
   render() {
+    const { uploading, imgSource, progress, images } = this.state;
+    const windowWidth = Dimensions.get('window').width;
+    const disabledStyle = uploading ? styles.disabledBtn : {};
+    const actionBtnStyles = [styles.btn, disabledStyle];
+    // AsyncStorage.clear();
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native + Firebase!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        {!firebase.apps.length && (
-          <Text style={styles.instructions}>
-            {`\nYou currently have no Firebase apps registered, this most likely means you've not downloaded your project credentials. Visit the link below to learn more. \n\n ${firebaseCredentials}`}
-          </Text>
-        )}
+      <View>
+        <ScrollView>
+          <View style={styles.container}>
+            <TouchableOpacity
+              style={actionBtnStyles}
+              onPress={this.pickImage}
+              disabled={uploading}
+            >
+              <View>
+                <Text style={styles.btnTxt}>Pick image</Text>
+              </View>
+            </TouchableOpacity>
+            {/** Display selected image */}
+            {imgSource !== '' && (
+              <View>
+                <Image source={imgSource} style={styles.image} />
+                {uploading && (
+                  <View
+                    style={[styles.progressBar, { width: `${progress}%` }]}
+                  />
+                )}
+                <TouchableOpacity
+                  style={actionBtnStyles}
+                  onPress={this.uploadImage}
+                  disabled={uploading}
+                >
+                  <View>
+                    {uploading ? (
+                      <Text style={styles.btnTxt}>Uploading ...</Text>
+                    ) : (
+                      <Text style={styles.btnTxt}>Upload image</Text>
+                    )}
+                  </View>
+                </TouchableOpacity>
+              </View>
+            )}
+
+            <View>
+              <Text
+                style={{
+                  fontWeight: '600',
+                  paddingTop: 20,
+                  alignSelf: 'center'
+                }}
+              >
+                {images.length > 0
+                  ? 'Your uploaded images'
+                  : 'There is no image you uploaded'}
+              </Text>
+            </View>
+            <FlatList
+              numColumns={2}
+              style={{ marginTop: 20 }}
+              data={images}
+              renderItem={({ item: image, index }) => (
+                <ImageRow
+                  windowWidth={windowWidth}
+                  image={image}
+                  popImage={() => this.removeImage(index)}
+                />
+              )}
+              keyExtractor={index => index}
+            />
+          </View>
+        </ScrollView>
       </View>
     );
   }
@@ -50,18 +201,194 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'column',
+    backgroundColor: '#ffffff',
+    marginTop: 20,
+    paddingLeft: 5,
+    paddingRight: 5
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  btn: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderRadius: 20,
+    backgroundColor: 'rgb(3, 154, 229)',
+    marginTop: 20,
+    alignItems: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  disabledBtn: {
+    backgroundColor: 'rgba(3,155,229,0.5)'
   },
+  btnTxt: {
+    color: '#fff'
+  },
+  image: {
+    marginTop: 20,
+    minWidth: 200,
+    height: 200,
+    resizeMode: 'contain',
+    backgroundColor: '#ccc',
+  },
+  img: {
+    flex: 1,
+    height: 100,
+    margin: 5,
+    resizeMode: 'contain',
+    borderWidth: 1,
+    borderColor: '#eee',
+    backgroundColor: '#ccc'
+  },
+  progressBar: {
+    backgroundColor: 'rgb(3, 154, 229)',
+    height: 3,
+    shadowColor: '#000',
+  }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///functional component example
+// import React, { useState } from 'react';
+// import { StyleSheet, Text, View, Button } from 'react-native';
+// import firebase from 'react-native-firebase';
+// const { app } = firebase.storage();
+
+// export default function App() {
+// 	const [count, setCount] = useState(0);
+//   alert(JSON.stringify(app));
+//   return (
+      
+//       <View style={styles.container}>
+//         <Text>You clicked {count} times.</Text>
+//         <Button
+//           onPress={() => setCount(count + 1)}
+//           title="Click me"
+//           color="red"
+//           accessibilityLabel="Click this button to increase count"
+//         />
+//       </View>
+//     );
+// }
+
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		justifyContent: 'center',
+// 		alignItems: 'center',
+// 		backgroundColor: '#F5FCFF'
+// 	},
+// 	welcome: {
+// 		fontSize: 20,
+// 		textAlign: 'center',
+// 		margin: 10
+// 	},
+// 	instructions: {
+// 		textAlign: 'center',
+// 		color: '#333333',
+// 		marginBottom: 5
+// 	}
+// });
+
+
+
+
+
+
+// /**
+//  * Sample React Native App with Firebase
+//  * https://github.com/invertase/react-native-firebase
+//  *
+//  * @format
+//  * @flow
+//  */
+
+// import React, { Component } from 'react';
+// import { Platform, StyleSheet, Text, View } from 'react-native';
+// import ContextState from './context_state_config';
+// import firebase from '@react-native-firebase/app';
+
+// // TODO(you): import any additional firebase services that you require for your app, e.g for auth:
+// //    1) install the npm package: `yarn add @react-native-firebase/auth@alpha` - you do not need to
+// //       run linking commands - this happens automatically at build time now
+// //    2) rebuild your app via `yarn run run:android` or `yarn run run:ios`
+// //    3) import the package here in your JavaScript code: `import '@react-native-firebase/auth';`
+// //    4) The Firebase Auth service is now available to use here: `firebase.auth().currentUser`
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu',
+// });
+
+// const firebaseCredentials = Platform.select({
+//   ios: 'https://invertase.link/firebase-ios',
+//   android: 'https://invertase.link/firebase-android',
+// });
+
+// // type Props = {};
+
+// // export default class App extends Component<Props> {
+// //   render() {
+// //     return (
+// //       <View style={styles.container}>
+// //         <Text style={styles.welcome}>Welcome to React Native + Firebase!</Text>
+// //         <Text style={styles.instructions}>To get started, edit App.js</Text>
+// //         <Text style={styles.instructions}>{instructions}</Text>
+// //         {!firebase.apps.length && (
+// //           <Text style={styles.instructions}>
+// //             {`\nYou currently have no Firebase apps registered, this most likely means you've not downloaded your project credentials. Visit the link below to learn more. \n\n ${firebaseCredentials}`}
+// //           </Text>
+// //         )}
+// //       </View>
+// //     );
+// //   }
+// // }
+
+
+
+// const App = () => {
+
+//     return(
+//       <div>
+//      	 <ContextState />
+//       </div>
+//     )
+// }
+
+// export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
